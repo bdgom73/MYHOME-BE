90a71569b65dae37a2e0ceb1df9e4938bc7c0c81
bdgom <bdgom73@naver.com>
2021-06-30 14:38:41 +0900

    Reupdate

--- src/main/java/com/myhome/server/ServerApplication.java
+++ src/main/java/com/myhome/server/ServerApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
-
+//extends SpringBootServletInitializer
 @SpringBootApplication
-public class ServerApplication extends SpringBootServletInitializer {
+public class ServerApplication  {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
-		return applicationBuilder.sources(ServerApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
+//		return applicationBuilder.sources(ServerApplication.class);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
